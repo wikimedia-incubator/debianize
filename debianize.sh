a05c7ae6b8714e67a441efcd50be5dac8a2a612b
Diederik van Liere <dvanliere@wikimedia.org>
2012-10-16 13:43:47 +0000

    More robust error handling

--- debianize.sh
+++ debianize.sh
@@ -5,29 +5,68 @@
 set -e
 set -o pipefail
 
+# Set env variables
 if [ -z "$DEBFULLNAME" ]
 	then export DEBFULLNAME="Diederik van Liere"
 fi
 
+
+# Determine package
+if [ $(git rev-parse --is-bare-repository) = true ]
+then
+    REPOSITORY_BASENAME=$(basename "$PWD")
+else
+    REPOSITORY_BASENAME=$(basename $(readlink -nf "$PWD"/..))
+fi
+
+cd ..
+PACKAGE=${PWD##*/}
+echo REPOSITORY_BASENAME is $REPOSITORY_BASENAME
+echo PACKAGE is $PACKAGE
+if [ $REPOSITORY_BASENAME != $PACKAGE ]
+then
+	echo 'Start debianize.sh either in the debianize folder or in the root of your git repository.';
+	exit 1;
+fi
+
+git status | grep "Changed but not updated"
+if [ $? == 0 ]
+then
+	echo 'Please be aware that you have uncommited changes in your git repo.'
+	echo 'Continuening with the building the Debian package means that your'
+	echo 'your package will not be entirely up-to-date.'
+fi
+
+echo 'Building package for '$PACKAGE
+
+
 VERSION=`git describe | awk -F'-g[0-9a-fA-F]+' '{print $1}' | sed -e 's/\-/./g' `
 MAIN_VERSION=`git describe --abbrev=0`
 
-PACKAGE=${PWD##*/}
-echo 'Building package for ' + $PACKAGE
 
 tar -cvf $PACKAGE.tar --exclude-from=exclude .
 mv $PACKAGE.tar ../
 cd ..
-rm -rf $PACKAGE-${VERSION}
+
+# this file is not always present and so the absence of this file should not kill this script.
+
+rm -rf $PACKAGE-${VERSION} 2> /dev/null || true
+
+# Create a tar file of the package
 mkdir $PACKAGE-${VERSION}
 tar -C $PACKAGE-${VERSION} -xvf $PACKAGE.tar
 
-rm ${PACKAGE}_${VERSION}.orig.tar.gz
+rm ${PACKAGE}_${VERSION}.orig.tar.gz  2> /dev/null || true
 
 cd $PACKAGE-${VERSION}
 
-VERSION=$VERSION perl -pi -e 's/VERSION=".*";/VERSION="$ENV{VERSION}";/' src/udp-filter.c
+# Replace version place holder with current versio number
+echo `pwd`
+echo 'Replacing version placeholders';
 
+VERSION=$VERSION perl -pi -e 's/VERSION=".*";/VERSION="$ENV{VERSION}";/' src/collector.c
+
+echo 'Launching package builder';
 mkdir m4
 dh_make -c gpl2 -e dvanliere@wikimedia.org -s --createorig -p $PACKAGE\_${VERSION}
 cd debian
